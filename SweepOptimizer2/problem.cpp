3a41168dd84eac8e74b2c6ad1ad75e7f6a3f9ba2
YSRKEN <ysr.ken@gmail.com>
2017-03-27 10:54:07 +0900

    読み込み方法を少し変更

--- SweepOptimizer2/problem.cpp
+++ SweepOptimizer2/problem.cpp
@@ -10,18 +10,32 @@ enum class StaffTask { Free, NonFree };
 Problem::Problem(const char file_name[]) {
 	// 読み込み用定数
 	enum class FloorObject { Dirty, Clean, Boy, Girl, Robot, Pool, Apple, Bottle, Dust, Recycle, Block };
-
+	// ファイルを読み込む
 	std::ifstream ifs(file_name);
 	// 盤面サイズを読み込む
 	ifs >> size_x_ >> size_y_;
-	// 盤面を読み込みつつ、各メンバ変数に代入していく
+	// 各メンバ変数を初期化
 	floor_dirty_.set_zero();
+	floor_pool_.set_zero();
+	floor_apple_.set_zero();
+	floor_bottle_.set_zero();
+	floor_around_dust_.set_zero();
+	floor_around_recycle_.set_zero();
+	// テンポラリ配列に盤面を読み込む
+	vector<FloorObject> temp_floor(size_x_ * size_y_);
+	for (size_t y = 0; y < size_y_; ++y) {
+		for (size_t x = 0; x < size_x_; ++x) {
+			size_t temp;
+			ifs >> temp;
+			size_t point = y * size_x_ + x;
+			temp_floor[point] = static_cast<FloorObject>(temp);
+		}
+	}
+	// テンポラリ配列を解析し、各メンバ変数に代入していく
 	for (size_t y = 0; y < size_y_; ++y) {
 		for (size_t x = 0; x < size_x_; ++x) {
 			size_t point = y * size_x_ + x;
-			size_t temp;
-			ifs >> temp;
-			switch (static_cast<FloorObject>(temp)) {
+			switch (temp_floor[point]) {
 			case FloorObject::Dirty:
 				floor_dirty_.set_bit(point);
 				break;
